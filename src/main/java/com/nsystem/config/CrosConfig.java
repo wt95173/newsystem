21ab3c939bc6b77cc32847ce71c4861d7cfc2ca6
“wt95173” <wt95173@163.com>
2021-01-28 23:39:00 +0800

    更新@Configuration

--- src/main/java/com/nsystem/config/CrosConfig.java
+++ src/main/java/com/nsystem/config/CrosConfig.java
@@ -1,8 +1,10 @@
 package com.nsystem.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CrosConfig implements WebMvcConfigurer {
 
     @Override
