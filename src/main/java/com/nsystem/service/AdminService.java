bce6cb965099ba62e123f662e9b0f407d34e8bb3
“wt95173” <wt95173@163.com>
2021-01-30 22:57:44 +0800

    添加管理员指派助教功能

--- src/main/java/com/nsystem/service/AdminService.java
+++ src/main/java/com/nsystem/service/AdminService.java
@@ -1,9 +1,16 @@
 package com.nsystem.service;
 
+import com.nsystem.entity.Choice;
 import com.nsystem.vo.LoginVo;
+import com.nsystem.vo.StudentVo;
+import com.nsystem.vo.TableVo;
 
 import javax.servlet.http.HttpSession;
 
 public interface AdminService {
     public LoginVo getUserName(HttpSession session);
+
+    public TableVo<StudentVo> getStudent(Integer page,Integer limit);
+
+    public int appointStudent(Integer studentId,Integer courseId);
 }
--- src/main/java/com/nsystem/service/impl/AdminServiceImpl.java
+++ src/main/java/com/nsystem/service/impl/AdminServiceImpl.java
@@ -1,14 +1,34 @@
 package com.nsystem.service.impl;
 
-import com.nsystem.entity.LoginInformation;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.nsystem.entity.*;
+import com.nsystem.mapper.EvaluationTableMapper;
+import com.nsystem.mapper.MajorMapper;
+import com.nsystem.mapper.StudentMapper;
 import com.nsystem.service.AdminService;
 import com.nsystem.vo.LoginVo;
+import com.nsystem.vo.StudentVo;
+import com.nsystem.vo.TableVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private EvaluationTableMapper evaluationTableMapper;
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    @Autowired
+    private MajorMapper majorMapper;
 
     @Override
     public LoginVo getUserName(HttpSession session) {
@@ -16,5 +36,48 @@ public class AdminServiceImpl implements AdminService {
         LoginVo loginVo=new LoginVo();
         loginVo.setUsername(loginInformation.getUserName());
         return loginVo;
+    }
+
+    @Override
+    public TableVo<StudentVo> getStudent(Integer page, Integer limit) {
+        TableVo tableVo=new TableVo();
+        tableVo.setCode(0);
+        tableVo.setMsg("");
+        List<StudentVo> studentVoList=new ArrayList<>();
+
+        QueryWrapper wrapper=new QueryWrapper();
+        List<EvaluationTable> evaluationTableList=evaluationTableMapper.selectList(null);
+        for(EvaluationTable evaluationTable:evaluationTableList){
+            wrapper.ne("student_id",evaluationTable.getStudentId());
+        }
+        tableVo.setCount(studentMapper.selectCount(wrapper));
+        IPage<Student> studentIPage=new Page<>(page,limit);
+        IPage<Student> result=studentMapper.selectPage(studentIPage,wrapper);
+        List<Student> studentList=result.getRecords();
+
+        for(Student student:studentList){
+            StudentVo studentVo=new StudentVo();
+            BeanUtils.copyProperties(student,studentVo);
+            QueryWrapper wrapper1 = new QueryWrapper();
+            wrapper1.eq("major_id", student.getMajorId());
+            studentVo.setMajorName(majorMapper.selectOne(wrapper1).getMajorName());
+            if (student.getSex().equals(0)) {
+                studentVo.setSex("男");
+            } else {
+                studentVo.setSex("女");
+            }
+            studentVoList.add(studentVo);
+        }
+        tableVo.setData(studentVoList);
+        return tableVo;
+    }
+
+    public int appointStudent(Integer studentId,Integer courseId){
+        EvaluationTable evaluationTable=new EvaluationTable();
+        evaluationTable.setStudentId(studentId);
+        evaluationTable.setCourseId(courseId);
+        List<EvaluationTable> evaluationTableList=evaluationTableMapper.selectList(null);
+        evaluationTable.setEvaluationId(evaluationTableList.get(evaluationTableList.size()-1).getEvaluationId()+1);
+        return evaluationTableMapper.insert(evaluationTable);
     }
 }
