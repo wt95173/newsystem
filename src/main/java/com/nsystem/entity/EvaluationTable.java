add75e5f09a605f725278b28c45de53fbc214878
“wt95173” <wt95173@163.com>
2021-01-30 21:57:44 +0800

    添加教师评定功能

--- src/main/java/com/nsystem/entity/EvaluationTable.java
+++ src/main/java/com/nsystem/entity/EvaluationTable.java
@@ -1,9 +1,11 @@
 package com.nsystem.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class EvaluationTable {
+    @TableId
     private Integer evaluationId;
     private Integer studentId;
     private Integer courseId;
